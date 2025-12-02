4584a46e1b321e2f77a0a882ae697c2940e9343d
samir02987 <starg07515@gmail.com>
2025-12-02 16:30:42 +0530

    resolved conflicts

--- app/(main)/onboarding/_components/onboarding-form.jsx
+++ app/(main)/onboarding/_components/onboarding-form.jsx
@@ -3,13 +3,59 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { onboardingSchema } from "@/app/lib/schema";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
 
 const OnboardingForm = ({ industries }) => {
-  const form = useForm({
+
+  const [selectedIndustry, setselectedIndustry] = useState(null);
+  const router = useRouter(); 
+
+  const {register,
+        handleSubmit,
+        formState:{errors},
+        setValue,
+        watch,
+      } = useForm({
     resolver: zodResolver(onboardingSchema)
   })
   
-  return <div>OnboardingForm</div>;
+  return ( 
+  <div className="flex items-center justify-center bg-background">
+    <Card className="w-full max-w-lg mt-10 mx-2">
+      <CardHeader>
+        <CardTitle className='gradient-title text-4xl'>Complete Your Profile</CardTitle>
+        <CardDescription>
+          Select your industry to get personalized career insights and recommendations.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div>
+            <Label htmlFor='industry'>Industry</Label>
+            <Select>
+             <SelectTrigger className="w-[180px]">
+               <SelectValue placeholder="Theme" />
+             </SelectTrigger>
+             <SelectContent>
+               {industries.map((ind)=>{
+                return ( 
+                  <SelectItem value={ind.id} key={ind.id}>
+                    {ind.name}
+                  </SelectItem>
+                );
+               })}
+             </SelectContent>
+            </Select>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  </div>
+  );
 };
 
 export default OnboardingForm;
